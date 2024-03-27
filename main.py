e0729ac8660ab59582512a0f88beaef2c18689be
wgonzalez15 <157772718+wgonzalez15@users.noreply.github.com>
2024-03-27 04:35:20 +0000

    DONE

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ def main():
     result = []
 
     numbers = list(map(int, (input().split())))
-
+    start = 0
+    end = -1
+    
+    for i in range(len(numbers)//2):
+        result.append(numbers[start] + numbers[end])
+        start +=1
+        end -= 1
     """
     ########################################
     Code Your Program here
